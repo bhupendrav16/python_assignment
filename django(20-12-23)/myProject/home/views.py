ac92b44fbcd6c687ed6dfc1d569218a12c5b6162
bhupendrav16 <bhupendra.v.brudite@gmail.com>
2023-12-21 17:08:09 +0530

    django myproject

--- django(20-12-23)/myProject/home/views.py
+++ django(20-12-23)/myProject/home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
-
+weeksDays = ["Monday" , "Tuesday"]
+links_name = ["Google","youtube"]
+link_ref = ["https://www.google.com" , "https://www.youtube.com"]
 def hello_world(request):
     print("req")
     print(request)
@@ -12,4 +13,4 @@ def nav_bar(request):
     return render(request, "home/home.html")
 
 def login(request):
-    return render( request , 'home/login.html')
+    return render( request , 'home/login.html', {"name" : links_name ,"link_r":link_ref} )
