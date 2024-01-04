a36dc7cdfa737bc93d3dcb8399da28b8093867da
bhupendrav16 <bhupendra.v.brudite@gmail.com>
2024-01-04 22:22:09 +0530

    correct question8

--- advance/question8.py
+++ advance/question8.py
@@ -1,9 +1,9 @@
-def userdata( s):
+def userdata(s):
     new_s = s.split('0')
-    l = [ ele for ele in new_s if len(ele) !=0]
-    d = {"first_name" :  l[0] , "last_name":l[1] , "id":l[2]}
-    return d 
- 
+    l = [ele for ele in new_s if len(ele)!=0]
+    if l[2].isdigit():
+        return  {"first_name":l[0],"second_name": l[1], 'id':l[2]}
+    return {}
 s = input()
 print(userdata(s))
 
