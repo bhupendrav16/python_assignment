93de9864e9875d08997e3f4a67994acea0d47af8
bhupendrav16 <bhupendra.v.brudite@gmail.com>
2024-01-04 22:20:54 +0530

    corrected question2

--- medium/question2.py
+++ medium/question2.py
@@ -1,14 +1,5 @@
-def unique(arr1):
-    d = {}
-    for ele in arr1:
-        if ele in d:
-            d[ele] += 1
-        else:
-            d[ele] = 1
-    ans = []
-    for ele in d:
-        if ( d[ele] == 1):
-            ans.append(ele)
-    return ans
-arr1 = list(map(int,input().split()))
-print(unique(arr1))
+def unique_elements(arr1):
+    return list(set(arr1))
+
+arr1 = list(map(int, input().split()))
+print(unique_elements(arr1))
