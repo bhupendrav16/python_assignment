c5e5c5893d4499fb42ab625f27e6b7b3ff11f32c
bhupendrav16 <bhupendra.v.brudite@gmail.com>
2024-01-04 22:19:35 +0530

    corrected question11

--- basic/question11.py
+++ basic/question11.py
@@ -5,16 +5,7 @@ def single_digit(n):
         return 9 
     else:
         return ( n % 9)
-
-    # s = 0
-    # while ( n  > 0 or s > 9):
-    #     if n == 0 :
-    #         n = s 
-    #         s = 0
-    #     s += n% 10 
-    #     n = n//10
-    # return s
-n = int(input())
-print(single_digit(n))
+n = int(input("num = "))
+print(f"sum_of_digits = {single_digit(n)}")
 
 
